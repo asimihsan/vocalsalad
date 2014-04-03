51fb6c5e994acf362b28e3b4ae9ddc54214462ce
Asim Ihsan <asim.ihsan@youview.com>
2014-04-03 16:46:16 +0100

    handlers/run - add tiny test to check a run endpoint exists
    We're just POST'ing to '/api/v1/run' and checking the server doesn't
    throw an error. Not even testing the return, checking if it worked,
    etc.

--- vocalsalad/app.py
+++ vocalsalad/app.py
@@ -1,7 +1,9 @@
 import tornado.web
 
 from .handlers.ping import PingHandler
+from .handlers.run import RunHandler
 
 application = tornado.web.Application([
     (r"/ping", PingHandler),
+    (r"/api/v1/run", RunHandler),
 ])
