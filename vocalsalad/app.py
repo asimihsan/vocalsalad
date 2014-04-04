df4dbf8bd040ed5b31d772a51462620db753109f
Asim Ihsan <asim.ihsan@youview.com>
2014-04-04 09:59:23 +0100

    app - refactor to allow Tornado settings to be injected at runtime
    This is moving towards using dependency injection to allow testing
    of a persistent database. However this commit does not change any
    functionality.

--- vocalsalad/app.py
+++ vocalsalad/app.py
@@ -3,7 +3,9 @@ import tornado.web
 from .handlers.ping import PingHandler
 from .handlers.run import RunHandler
 
-application = tornado.web.Application([
-    (r"/ping", PingHandler),
-    (r"/api/v1/run", RunHandler),
-])
+
+def get_application(**settings):
+    return tornado.web.Application([
+        (r"/ping", PingHandler),
+        (r"/api/v1/run", RunHandler),
+    ], **settings)
--- vocalsalad/server.py
+++ vocalsalad/server.py
@@ -2,15 +2,17 @@ from __future__ import print_function
 
 import tornado.ioloop
 
-from vocalsalad.app import application
+from vocalsalad.app import get_application
 
 
 class Server(object):
-    def __init__(self, host, port):
+    def __init__(self, host, port, **application_settings):
         self.host = host
         self.port = port
+        self.application_settings = application_settings
 
     def start(self):
+        application = get_application(**self.application_settings)
         application.listen(self.port, self.host)
         tornado.ioloop.IOLoop.instance().start()
 
@@ -22,7 +24,8 @@ class Server(object):
 
 def main():
     print("woohoo i'm running!")
-    Server(host='0.0.0.0', port=20080).start()
+    Server(host='0.0.0.0',
+           port=20080).start()
 
 if __name__ == "__main__":
     main()
