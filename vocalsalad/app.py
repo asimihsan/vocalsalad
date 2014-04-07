923c19908626fab36b589c5f15d6de404fe5dcf4
Asim Ihsan <asim.ihsan@youview.com>
2014-04-07 18:04:15 +0100

    handlers/run - use hard-coded implementation to pass tests
    Next stage will be to use triangulation to drive out this particular
    hard-coded implementation. Still not sure how I'll be implementing
    process running but the simplest implementation would be to run them
    as children, so may start there.

--- vocalsalad/app.py
+++ vocalsalad/app.py
@@ -1,11 +1,12 @@
 import tornado.web
 
 from .handlers.ping import PingHandler
-from .handlers.run import RunHandler
+from .handlers.run import RunStartHandler, RunHandler
 
 
 def get_application(**settings):
     return tornado.web.Application([
         (r"/ping", PingHandler),
-        (r"/api/v1/run/(.*)", RunHandler),
+        (r"/api/v1/run/start", RunStartHandler),
+        (r"/api/v1/run/(.*?)/(.*)", RunHandler),
     ], **settings)
